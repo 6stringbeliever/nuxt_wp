82617d263d354551a44a5abce81910a49f21c94f
Shaun Kelly <shaunkelly@gmail.com>
2017-11-06 11:00:34 -0500

    feat: Add vuex store

---
+++ store/index.js
@@ -0,0 +1,38 @@
+import Vuex from 'vuex'
+import wp from '~/lib/wp'
+
+// Mutation Types
+export const types = {
+  SITE_DATA_UPDATE: 'SITE_DATA_UPDATE',
+  POST_LIST_UPDATE: 'POST_LIST_UPDATE',
+  CURRENT_POST_UPDATE: 'CURRENT_POST_UPDATE'
+}
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      site_data: {}
+    },
+    mutations: {
+      [types.SITE_DATA_UPDATE] (state, payload) {
+        state.site_data = { ...payload }
+      },
+      [types.POST_LIST_UPDATE] (state, payload) {
+        state.post_list = [ ...payload ]
+      },
+      [types.CURRENT_POST_UPDATE] (state, payload) {
+        state.current_post = { ...payload }
+      }
+    },
+    actions: {
+      nuxtServerInit ({ commit }) {
+        return wp.siteData()
+          .then(res => {
+            commit(types.SITE_DATA_UPDATE, res.site_data)
+          })
+      }
+    }
+  })
+}
+
+export default createStore
