7ba5295d34306f20d4804288da523bb16ce74747
Shaun Kelly <shaunkelly@gmail.com>
2017-11-21 15:00:09 -0500

    feat: Add header style

--- components/layout/BlogHeader.vue
+++ components/layout/BlogHeader.vue
@@ -1,7 +1,9 @@
 <template>
-  <header class="container">
-    <h1><nuxt-link to="/">{{ siteData.name }}</nuxt-link></h1>
-    <p>{{ siteData.description }}</p>
+  <header>
+    <div class="header-contents">
+      <h1><nuxt-link to="/">{{ siteData.name }}</nuxt-link></h1>
+      <p class="subhead">{{ siteData.description }}</p>
+    </div>
   </header>
 </template>
 
@@ -12,3 +14,43 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+header {
+  padding: 4rem 0 7rem 0;
+  background-image: linear-gradient(325deg, #000380 2%, #197849 100%);
+  box-shadow: inset 0 1px 3px 0 rgba(0,0,0,0.50);
+}
+
+h1 {
+  font-weight: 900;
+  font-size: 3rem;
+  text-shadow: 2px 4px 4px rgba(0,0,0,0.40);
+
+  a {
+    color: #FFFFFF;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+}
+
+.header-contents {
+  margin: 0 auto;
+  width: 90%;
+  max-width: 1080px;
+  padding: 0 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+}
+
+.subhead {
+  color: #ffffff;
+  font-weight: 600;
+  font-style: italic;
+}
+</style>
+
